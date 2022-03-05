8baa688652a5939f7179c416d9ee80065b536d56
root <root@DESKTOP-HIBFKE8.localdomain>
2022-03-05 18:37:36 -0500

    fleshed out rotate logic a bit more

--- tetris/ntris.py
+++ tetris/ntris.py
@@ -1,4 +1,4 @@
-from piece_data import rots, kick_kernals
+from piece_data import rots, kickstack
 
 import numpy as np
 from scipy.signal import correlate2d
@@ -10,8 +10,8 @@ and other linear algebra tricks to generate moves at a much higher
 speed.
 """
 
-TRIL = np.tril(np.ones((40, 40)), k=1)
-TRIU = np.triu(np.ones((40, 40)), k=1)
+TRIL = np.tril(np.ones((40, 40), dtype=int), k=1)
+TRIU = np.triu(np.ones((40, 40), dtype=int), k=1)
 POW = np.power(2, np.arange(40))
 
 class Tetris():
@@ -32,33 +32,34 @@ class Tetris():
         left = left @ TRIU[:10, :10]
         right = np.multiply(moves, POW[:10][::-1])
         right = right @ TRIL[:10, :10]
-        moves = np.clip(np.clip(right, 0, 1) + np.clip(left, 0, 1), 0, 1)
+        print(np.clip(left, 0, 1))
+        moves = np.clip(right, 0, 1) & np.clip(left, 0, 1)
         return moves
     
-    # please let me know if you know how this works, because i certainly don't
     def rotate(moves, full):
-        empty = 1 - full
-        rotates = np.zeros(empty.shape)
-        # loop through rotations
-        for i in range(1, 4):
-            mask = np.roll(moves, i, axis=0) - empty
-            # loop through states
-            # it's not isomorphic, but it's non-branching which eases computation
-            rmoves = np.array([correlate2d(mask[i], kick_kernals[str(j) + str((i + j) % 4)], fillvalue=0) for j in range(4)])[:,3:43, 3:13]
-            print(rmoves.shape)
-            rotates += np.clip(rmoves, 0, 1)
-        moves = np.clip(rotates + moves, 0, 1)
+        # loop through source and target rotations
+        newmoves = np.zeros(moves.shape)
+        for i in kickstack:
+            mask = 1 - full[i % 10]
+            move = moves[i // 10]
+            k = kickstack[i]
+            n, _, _ = k.shape
+            l = np.transpose(k, (0, 2, 1)) @ np.multiply(((k @ move) & mask), POW[:n, np.newaxis, np.newaxis])
+            newmoves[i % 10] += np.sum(k @ np.clip(l, 0, 1), axis=0)
+        print(newmoves)
+
+        moves = np.clip(newmoves + moves, 0, 1)
         return moves
         
     
     def generate_moves(board, piece): 
-        moves = np.zeros((4, 40, 10))
+        moves = np.zeros((4, 40, 10), dtype=int)
         moves[0][18][5] = 1
-        full = np.array([correlate2d(board, rots[piece][i], fillvalue=1)[1:41,1:11] for i in range(4)])
+        full = np.array([correlate2d(board, rots[piece][i], fillvalue=1)[1:41,1:11] for i in range(4)], dtype=int)
         full = np.clip(full, 0, 1)
-        moves = Tetris.rotate(moves, full)
         moves = Tetris.wiggle(moves, full)
         moves = Tetris.drop(moves, full)
+        Tetris.pretty_print(moves[0])
         moves = Tetris.rotate(moves, full)
         return moves
     
@@ -66,13 +67,12 @@ class Tetris():
         return np.zeros((40, 10))
 
     def pretty_print(board):
-        s = "\n".join(["".join(["  " if j == 0 else "██" for j in i])
+        s = "\n".join(["".join(["  " if j <= 0 else "██" for j in i])
                       for i in board[21:]])
         print(s)
 
 board = Tetris.empty_board()
 
 board[30, 5] = 1
-print(board)
 
 Tetris.pretty_print(Tetris.generate_moves(board, "j")[0])
